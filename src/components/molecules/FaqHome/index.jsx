af91df10a98f993a7f9427c5c16fbbf4da43b10f
Andreas-Yoseph-Liandy <samueljuan0619@gmail.com>
2022-12-05 14:57:48 +0700

    API FAQ

--- src/components/molecules/FaqHome/index.jsx
+++ src/components/molecules/FaqHome/index.jsx
@@ -1,179 +1,144 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import Accordion from "../../atoms/Accordion";
 import AccordionItem from "../../atoms/AccordionItem";
 
 const FaqHome = () => {
-	const [select, setSelect] = useState(1);
-	let allData = [];
-	const dataName = ["General", "Treatment", "Order", "Acne"];
-	const data = [
-		{
-			id: 1,
-			value: "General",
-		},
-		{
-			id: 2,
-			value: "Treatment",
-		},
-		{
-			id: 3,
-			value: "Order",
-		},
-		{
-			id: 4,
-			value: "Acne",
-		},
-	];
+  const [select, setSelect] = useState(1);
+  const [faqGeneral, setFaqGeneral] = useState([]);
+  const [faqTreatment, setFaqTreatment] = useState([]);
+  const [faqOrder, setFaqOrder] = useState([]);
+  const [faqAcne, setFaqAcne] = useState([]);
 
-	const dataGeneral = [
-		{
-			id: 1,
-			title: "Pertanyaan General",
-			des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit facilisi tincidunt pretium nec tortor in mi suscipit felis. Lectus adipiscing duis sagittis, diam cursus turpis ultricies nulla pellentesque. Id fames ipsum dolor mi. Dui faucibus enim sed praesent. Velit risus gravida ultrices mi suscipit arcu convallis. Nunc hendrerit nibh at odio fringilla nunc et. Dolor semper id.",
-		},
-		{
-			id: 2,
-			title: "Pertanyaan 2",
-			des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit facilisi tincidunt pretium nec tortor in mi suscipit felis. Lectus adipiscing duis sagittis, diam cursus turpis ultricies nulla pellentesque. Id fames ipsum dolor mi. Dui faucibus enim sed praesent. Velit risus gravida ultrices mi suscipit arcu convallis. Nunc hendrerit nibh at odio fringilla nunc et. Dolor semper id.",
-		},
-		{
-			id: 3,
-			title: "Pertanyaan 3",
-			des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit facilisi tincidunt pretium nec tortor in mi suscipit felis. Lectus adipiscing duis sagittis, diam cursus turpis ultricies nulla pellentesque. Id fames ipsum dolor mi. Dui faucibus enim sed praesent. Velit risus gravida ultrices mi suscipit arcu convallis. Nunc hendrerit nibh at odio fringilla nunc et. Dolor semper id.",
-		},
-		{
-			id: 4,
-			title: "Pertanyaan 4",
-			des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit facilisi tincidunt pretium nec tortor in mi suscipit felis. Lectus adipiscing duis sagittis, diam cursus turpis ultricies nulla pellentesque. Id fames ipsum dolor mi. Dui faucibus enim sed praesent. Velit risus gravida ultrices mi suscipit arcu convallis. Nunc hendrerit nibh at odio fringilla nunc et. Dolor semper id.",
-		},
-	];
+  let allData = [];
+  useEffect(() => {
+    const LinkAPIGeneral =
+      "https://6303aec40de3cd918b3c026a.mockapi.io/generalFAQ";
+    const LinkAPITreatment =
+      "https://6303aec40de3cd918b3c026a.mockapi.io/generalFAQ";
+    const LinkAPIOrder =
+      "https://6303aec40de3cd918b3c026a.mockapi.io/generalFAQ";
+    const LinkAPIAcne =
+      "https://6303aec40de3cd918b3c026a.mockapi.io/generalFAQ";
 
-	const dataTreatment = [
-		{
-			id: 1,
-			title: "Pertanyaan Treatment",
-			des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit facilisi tincidunt pretium nec tortor in mi suscipit felis. Lectus adipiscing duis sagittis, diam cursus turpis ultricies nulla pellentesque. Id fames ipsum dolor mi. Dui faucibus enim sed praesent. Velit risus gravida ultrices mi suscipit arcu convallis. Nunc hendrerit nibh at odio fringilla nunc et. Dolor semper id.",
-		},
-		{
-			id: 2,
-			title: "Pertanyaan 2",
-			des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit facilisi tincidunt pretium nec tortor in mi suscipit felis. Lectus adipiscing duis sagittis, diam cursus turpis ultricies nulla pellentesque. Id fames ipsum dolor mi. Dui faucibus enim sed praesent. Velit risus gravida ultrices mi suscipit arcu convallis. Nunc hendrerit nibh at odio fringilla nunc et. Dolor semper id.",
-		},
-		{
-			id: 3,
-			title: "Pertanyaan 3",
-			des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit facilisi tincidunt pretium nec tortor in mi suscipit felis. Lectus adipiscing duis sagittis, diam cursus turpis ultricies nulla pellentesque. Id fames ipsum dolor mi. Dui faucibus enim sed praesent. Velit risus gravida ultrices mi suscipit arcu convallis. Nunc hendrerit nibh at odio fringilla nunc et. Dolor semper id.",
-		},
-		{
-			id: 4,
-			title: "Pertanyaan 4",
-			des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit facilisi tincidunt pretium nec tortor in mi suscipit felis. Lectus adipiscing duis sagittis, diam cursus turpis ultricies nulla pellentesque. Id fames ipsum dolor mi. Dui faucibus enim sed praesent. Velit risus gravida ultrices mi suscipit arcu convallis. Nunc hendrerit nibh at odio fringilla nunc et. Dolor semper id.",
-		},
-	];
+    axios
+      .get(LinkAPIGeneral)
+      .then((response) => {
+        const convertData = Object.values(response.data);
+        setFaqGeneral(convertData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-	const dataAcne = [
-		{
-			id: 1,
-			title: "Pertanyaan Acne",
-			des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit facilisi tincidunt pretium nec tortor in mi suscipit felis. Lectus adipiscing duis sagittis, diam cursus turpis ultricies nulla pellentesque. Id fames ipsum dolor mi. Dui faucibus enim sed praesent. Velit risus gravida ultrices mi suscipit arcu convallis. Nunc hendrerit nibh at odio fringilla nunc et. Dolor semper id.",
-		},
-		{
-			id: 2,
-			title: "Pertanyaan 2",
-			des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit facilisi tincidunt pretium nec tortor in mi suscipit felis. Lectus adipiscing duis sagittis, diam cursus turpis ultricies nulla pellentesque. Id fames ipsum dolor mi. Dui faucibus enim sed praesent. Velit risus gravida ultrices mi suscipit arcu convallis. Nunc hendrerit nibh at odio fringilla nunc et. Dolor semper id.",
-		},
-		{
-			id: 3,
-			title: "Pertanyaan 3",
-			des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit facilisi tincidunt pretium nec tortor in mi suscipit felis. Lectus adipiscing duis sagittis, diam cursus turpis ultricies nulla pellentesque. Id fames ipsum dolor mi. Dui faucibus enim sed praesent. Velit risus gravida ultrices mi suscipit arcu convallis. Nunc hendrerit nibh at odio fringilla nunc et. Dolor semper id.",
-		},
-		{
-			id: 4,
-			title: "Pertanyaan 4",
-			des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit facilisi tincidunt pretium nec tortor in mi suscipit felis. Lectus adipiscing duis sagittis, diam cursus turpis ultricies nulla pellentesque. Id fames ipsum dolor mi. Dui faucibus enim sed praesent. Velit risus gravida ultrices mi suscipit arcu convallis. Nunc hendrerit nibh at odio fringilla nunc et. Dolor semper id.",
-		},
-	];
+    axios
+      .get(LinkAPITreatment)
+      .then((response) => {
+        const convertData = Object.values(response.data);
+        setFaqTreatment(convertData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-	const dataOrder = [
-		{
-			id: 1,
-			title: "Pertanyaan Order",
-			des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit facilisi tincidunt pretium nec tortor in mi suscipit felis. Lectus adipiscing duis sagittis, diam cursus turpis ultricies nulla pellentesque. Id fames ipsum dolor mi. Dui faucibus enim sed praesent. Velit risus gravida ultrices mi suscipit arcu convallis. Nunc hendrerit nibh at odio fringilla nunc et. Dolor semper id.",
-		},
-		{
-			id: 2,
-			title: "Pertanyaan 2",
-			des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit facilisi tincidunt pretium nec tortor in mi suscipit felis. Lectus adipiscing duis sagittis, diam cursus turpis ultricies nulla pellentesque. Id fames ipsum dolor mi. Dui faucibus enim sed praesent. Velit risus gravida ultrices mi suscipit arcu convallis. Nunc hendrerit nibh at odio fringilla nunc et. Dolor semper id.",
-		},
-		{
-			id: 3,
-			title: "Pertanyaan 3",
-			des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit facilisi tincidunt pretium nec tortor in mi suscipit felis. Lectus adipiscing duis sagittis, diam cursus turpis ultricies nulla pellentesque. Id fames ipsum dolor mi. Dui faucibus enim sed praesent. Velit risus gravida ultrices mi suscipit arcu convallis. Nunc hendrerit nibh at odio fringilla nunc et. Dolor semper id.",
-		},
-		{
-			id: 4,
-			title: "Pertanyaan 4",
-			des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit facilisi tincidunt pretium nec tortor in mi suscipit felis. Lectus adipiscing duis sagittis, diam cursus turpis ultricies nulla pellentesque. Id fames ipsum dolor mi. Dui faucibus enim sed praesent. Velit risus gravida ultrices mi suscipit arcu convallis. Nunc hendrerit nibh at odio fringilla nunc et. Dolor semper id.",
-		},
-	];
+    axios
+      .get(LinkAPIOrder)
+      .then((response) => {
+        const convertData = Object.values(response.data);
+        setFaqOrder(convertData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-	switch (select) {
-		case 1:
-			allData = dataGeneral;
-			break;
-		case 2:
-			allData = dataTreatment;
-			break;
-		case 3:
-			allData = dataAcne;
-			break;
-		case 4:
-			allData = dataOrder;
-			break;
-		default:
-			allData = allData = dataGeneral;
-			break;
-	}
+    axios
+      .get(LinkAPIAcne)
+      .then((response) => {
+        const convertData = Object.values(response.data);
+        setFaqAcne(convertData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-	console.log();
+  const dataName = ["General", "Treatment", "Order", "Acne"];
+  const data = [
+    {
+      id: 1,
+      value: "General",
+    },
+    {
+      id: 2,
+      value: "Treatment",
+    },
+    {
+      id: 3,
+      value: "Order",
+    },
+    {
+      id: 4,
+      value: "Acne",
+    },
+  ];
 
-	return (
-		<div>
-			<p className="text-center font-bold text-5xl">FAQ&apos;s</p>
+  switch (select) {
+    case 1:
+      allData = faqGeneral;
+      break;
+    case 2:
+      allData = faqTreatment;
+      break;
+    case 3:
+      allData = faqOrder;
+      break;
+    case 4:
+      allData = faqAcne;
+      break;
+    default:
+      allData = allData = faqGeneral;
+      break;
+  }
 
-			<div className="relative flex flex-1 flex-row overflow-x-scroll overflow-y-hidden md:justify-between text-[32px] text-white mt-[96px] font-playfair">
-				{data.map((x, y) => {
-					return (
-						<div
-							key={x.id}
-							className={`px-10 py-10 mr-2 md:mr-0 md:w-[285px] md:h-[285px] hover:bg-[#1B3E6680] rounded-lg flex justify-center items-center cursor-pointer ${
-								select === x.id ? "bg-[#1B3E6680]" : "bg-[#A3B7B380]"
-							}`}
-							onClick={() => setSelect(y + 1)}
-						>
-							{x.value}
-						</div>
-					);
-				})}
-			</div>
-			<div className="mt-[47px] font-playfair">
-				<p className="font-bold text-[32px]">{dataName[select - 1]}</p>
-				<div className="w-full border-b-2 border-[#1B3E66]"></div>
-			</div>
+  console.log();
 
-			<Accordion>
-				{(allData || []).map((x) => {
-					return (
-						<AccordionItem
-							title={x.title}
-							content={<div className={"text-lg mt-[24px]"}>{x.des}</div>}
-							key={x.id}
-						/>
-					);
-				})}
-			</Accordion>
-		</div>
-	);
+  return (
+    <div>
+      <p className="text-center font-bold text-5xl">FAQ&apos;s</p>
+
+      <div className="relative flex flex-1 flex-row overflow-x-scroll overflow-y-hidden md:justify-between text-[32px] text-white mt-[96px] font-playfair">
+        {data.map((x, y) => {
+          return (
+            <div
+              key={x.id}
+              className={`px-10 py-10 mr-2 md:mr-0 md:w-[285px] md:h-[285px] hover:bg-[#1B3E6680] rounded-lg flex justify-center items-center cursor-pointer ${
+                select === x.id ? "bg-[#1B3E6680]" : "bg-[#A3B7B380]"
+              }`}
+              onClick={() => setSelect(y + 1)}
+            >
+              {x.value}
+            </div>
+          );
+        })}
+      </div>
+      <div className="mt-[47px] font-playfair">
+        <p className="font-bold text-[32px]">{dataName[select - 1]}</p>
+        <div className="w-full border-b-2 border-[#1B3E66]"></div>
+      </div>
+
+      <Accordion>
+        {(allData || []).map((x) => {
+          return (
+            <AccordionItem
+              title={x.title}
+              content={<div className={"text-lg mt-[24px]"}>{x.des}</div>}
+              key={x.id}
+            />
+          );
+        })}
+      </Accordion>
+    </div>
+  );
 };
 
 export default FaqHome;
